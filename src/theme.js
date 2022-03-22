213ad0bf5bf5cc68fc21c6fbcaaf6880d4ac1c35
OrpheusOnApollo <3007446+praveenjayakody@users.noreply.github.com>
2022-03-22 19:21:43 +0530

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,7 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/core/styles';
 import { yellow, orange } from '@material-ui/core/colors';
 
-const theme = createMuiTheme({
+const theme = createTheme({
   palette: {
     primary: orange,
     secondary: yellow,
